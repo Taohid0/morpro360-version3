83c4abd24c4e3fe696e1a7c3e752cbf3cce2e66e
Taohidul Islam <taohidulii@gmail.com>
2019-04-03 15:17:32 +0600

    lastName made false in admin model:

--- models/admin.js
+++ models/admin.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     "lastName":{
       type: DataTypes.STRING,
-      allowNull:false,
+      allowNull:true,
     },
     "phone": {
       type:DataTypes.STRING,
